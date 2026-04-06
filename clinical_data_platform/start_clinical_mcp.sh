56669d019ea220d4e1af281b98e9f816be5f743e
SamuelXing7235 <02092101@163.com>
2026-04-06 15:48:43 +0800

    Delete clinical_data_platform directory

--- clinical_data_platform/start_clinical_mcp.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-source /home/lxing/miniconda3/etc/profile.d/conda.sh
-conda activate dataana
-
-# 直接用你成功运行的命令！！！
-exec strands-agents-sops mcp --sop-paths /home/lxing/agent-sop/clinical_data_platform
